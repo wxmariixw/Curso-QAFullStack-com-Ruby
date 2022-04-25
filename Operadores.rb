f54dc26c64bf1368ab6235b680325d6995cbb519
Mariana Freitas de Abreu <marisftsabreu@gmail.com>
2022-04-25 01:23:51 -0300

    Adicionando primeiros arquivos

---
+++ Operadores.rb
@@ -0,0 +1,22 @@
+#numero1 = 0
+#numero2 = 0
+#total = 0
+
+# Todo input vem como string
+#puts 'informe o número 1'
+#numero1 = gets.chomp.to_i
+
+#puts 'Informe o número 2'
+#numero2 = gets.chomp.to_i
+
+# Operadores matemáticos
+# total = numero1 + numero2
+# total = numero1 - numero2
+# total = numero1 * numero2
+# total = numero1 / numero2
+# puts total
+
+v1 = 10
+v2 = 11
+
+puts v1 > v2
---
+++ TiposDeDados.rb
@@ -0,0 +1,20 @@
+# -- Tipos Ruby --
+# O tipo de uma variável é definido no momento em que o mesmo recebe valor
+
+#string
+produto = 'Macbook Air'
+preco = 5999.99
+quantidade = 10
+disponivel = true
+
+puts produto
+puts preco
+puts quantidade
+puts disponivel
+
+quantidade = '9'
+
+puts produto.class
+puts preco.class
+puts quantidade.class
+puts disponivel.class
