ab052f13c47d868bd35eb4f30e3878f0f5ad0ecc
Mariana Freitas de Abreu <marisftsabreu@gmail.com>
2022-04-25 02:09:31 -0300

    Adicionando novas anotações

---
+++ Loops.rb
@@ -0,0 +1,20 @@
+# 5.times do |i|
+#     puts 'Repetindo a mensagem ' + i.to_s + ' vez(es).'
+# end
+
+# init = 0
+
+# while init <= 10 do
+#     puts 'Repetindo a mensagem ' + init.to_s + ' vez(es).'
+#     init += 1
+# end
+
+# for item in (1...11)
+#     puts 'Repetindo a mensagem ' + item.to_s + ' vez(es).'
+# end
+
+vingadores = ['Ironman', 'Hulk', 'Spiderman', 'Thor']
+
+vingadores.each do |v|
+    puts v
+end
---
+++ metodos.rb
@@ -0,0 +1,21 @@
+def diga_ola(nome)
+ puts 'Olá, ' + nome
+end
+
+diga_ola("Mariana")
+
+# def retorna_nome
+#    "Fernando"
+# end
+
+# resultado = retorna_nome
+# puts resultado
+
+# def soma(v1, v2)
+#    total = v1 + v2
+#    total
+# end
+
+# res = soma(10,12)
+# puts res
+
